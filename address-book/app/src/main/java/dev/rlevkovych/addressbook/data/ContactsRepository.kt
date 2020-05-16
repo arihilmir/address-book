a31ef31b78c26a8926191e8efbda8a4ea1eab859
Yaryna <p.yaryna@gmail.com>
2020-05-16 20:03:54 +0300

    change insert and update methods

--- address-book/app/src/main/java/dev/rlevkovych/addressbook/data/ContactsRepository.kt
+++ address-book/app/src/main/java/dev/rlevkovych/addressbook/data/ContactsRepository.kt
@@ -1,5 +1,6 @@
 package dev.rlevkovych.addressbook.data
 
+import android.os.AsyncTask
 import androidx.lifecycle.LiveData
 import dev.rlevkovych.addressbook.data.source.local.ContactsDao
 
@@ -10,11 +11,35 @@ class ContactsRepository(private val contactsDao: ContactsDao) {
         return contactsDao.getContactsById(id)
     }
 
+//    fun insert(contact: Contact) {
+//        contactsDao.insert(contact)
+//    }
+
     fun insert(contact: Contact) {
-        contactsDao.insert(contact)
+        insertAsyncTask(contactsDao).execute(contact)
     }
 
+//    fun update(contact: Contact) {
+//        contactsDao.updateTask(contact)
+//    }
+
     fun update(contact: Contact) {
-        contactsDao.updateTask(contact)
+        updateAsyncTask(contactsDao).execute(contact)
+    }
+
+    private class insertAsyncTask internal constructor(private val insertcontactsDao: ContactsDao) : AsyncTask<Contact, Void, Void>() {
+
+        override fun doInBackground(vararg params: Contact): Void? {
+            insertcontactsDao.insert(params[0])
+            return null
+        }
+    }
+
+    private class updateAsyncTask internal constructor(private val updatecontactsDao: ContactsDao) : AsyncTask<Contact, Void, Void>() {
+
+        override fun doInBackground(vararg params: Contact): Void? {
+            updatecontactsDao.updateTask(params[0])
+            return null
+        }
     }
 }
