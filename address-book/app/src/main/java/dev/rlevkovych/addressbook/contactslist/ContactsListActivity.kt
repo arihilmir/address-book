5df500a7669923521a92fb9ab3a91af784d3c230
Calvados <flamaster0807@gmail.com>
2020-05-17 19:26:32 +0300

    implemented dataConfig option respecting on ContactsListScreen

--- address-book/app/src/main/java/dev/rlevkovych/addressbook/contactslist/ContactsListActivity.kt
+++ address-book/app/src/main/java/dev/rlevkovych/addressbook/contactslist/ContactsListActivity.kt
@@ -1,5 +1,6 @@
 package dev.rlevkovych.addressbook.contactslist
 
+import android.app.Application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +10,8 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -27,7 +30,16 @@ class ContactsListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts_list)
 
-        viewModel = ViewModelProviders.of(this).get(ContactsListViewModel::class.java)
+        val configValue = intent.getStringExtra("dataConfig")
+        var option: AccountDisplayOption
+        if (configValue == null)
+            option = AccountDisplayOption.All
+        else if (configValue == "all")
+            option = AccountDisplayOption.All
+        else
+            option = AccountDisplayOption.Groups
+
+        viewModel = ViewModelProviders.of(this, CustomViewModelFactory(this.getApplication(), option)).get(ContactsListViewModel::class.java)
         viewModel.allContacts.observe(this, Observer { items ->
             if (items.isNotEmpty()) {
                 contactId = items.first().id
@@ -56,5 +68,12 @@ class ContactsListActivity : AppCompatActivity() {
 
             startActivity(intent)
         }
+    }
+
+    class CustomViewModelFactory(private val application: Application, private val option: AccountDisplayOption) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return ContactsListViewModel(application, option) as T
+        }
+
     }
 }
