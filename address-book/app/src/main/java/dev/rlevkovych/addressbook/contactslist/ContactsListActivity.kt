906de9c06198f274b5d06002a30dff27ba8324d9
Calvados <flamaster0807@gmail.com>
2020-05-17 15:38:54 +0300

    commited implementation files

--- address-book/app/src/main/java/dev/rlevkovych/addressbook/contactslist/ContactsListActivity.kt
+++ address-book/app/src/main/java/dev/rlevkovych/addressbook/contactslist/ContactsListActivity.kt
@@ -4,17 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dev.rlevkovych.addressbook.R
 import dev.rlevkovych.addressbook.contactsdetail.ContactsDetailActivity
+import dev.rlevkovych.addressbook.editcontact.EditContactActivity
+import kotlinx.android.synthetic.main.activity_contacts_list.*
 
 class ContactsListActivity : AppCompatActivity() {
 
     private lateinit var viewModel: ContactsListViewModel
     private lateinit var contactId: String
+    private lateinit var contactsAdapter: ContactsRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +30,13 @@ class ContactsListActivity : AppCompatActivity() {
         viewModel.allContacts.observe(this, Observer { items ->
             if (items.isNotEmpty()) {
                 contactId = items.first().id
+
+                contactsRecyclerView.apply {
+                    layoutManager = LinearLayoutManager(this@ContactsListActivity)
+                    contactsAdapter = ContactsRecyclerAdapter()
+                    adapter = contactsAdapter
+                }
+                contactsAdapter.submitList(items)
             } else {
                 Toast.makeText(this, "No data", Toast.LENGTH_LONG).show()
             }
@@ -31,8 +44,7 @@ class ContactsListActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.openContact)
         button.setOnClickListener {
-            val intent = Intent(this, ContactsDetailActivity::class.java).apply {
-                putExtra("contactId", contactId)
+            val intent = Intent(this, EditContactActivity::class.java).apply {
             }
 
             startActivity(intent)
--- address-book/app/src/main/java/dev/rlevkovych/addressbook/contactslist/ContactsRecyclerAdapter.kt
+++ address-book/app/src/main/java/dev/rlevkovych/addressbook/contactslist/ContactsRecyclerAdapter.kt
@@ -1,2 +1,46 @@
 package dev.rlevkovych.addressbook.contactslist
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import dev.rlevkovych.addressbook.R
+import dev.rlevkovych.addressbook.data.Contact
+import kotlinx.android.synthetic.main.layout_contact_list_item.view.*
+
+class ContactsRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var items : List<Contact> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ContactViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.layout_contact_list_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder) {
+            is ContactViewHolder -> {
+                holder.bind(items.get(position))
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun submitList(contactList: List<Contact>) {
+        items = contactList
+    }
+
+    class ContactViewHolder constructor(
+            itemView: View
+    ): RecyclerView.ViewHolder(itemView) {
+        val title = itemView.tv_label
+
+        fun bind(contact: Contact) {
+            title.setText(contact.name);
+        }
+    }
+
+}
