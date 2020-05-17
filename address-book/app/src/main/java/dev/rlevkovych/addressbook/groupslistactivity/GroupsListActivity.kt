903d394dfb3df3795d20b54f80eb8ff7b3d72fde
Roman Levkovych <rlevkovych098@gmail.com>
2020-05-17 21:03:07 +0300

    Basic navigation + minor refactor

--- address-book/app/src/main/java/dev/rlevkovych/addressbook/groupslistactivity/GroupsListActivity.kt
+++ address-book/app/src/main/java/dev/rlevkovych/addressbook/groupslistactivity/GroupsListActivity.kt
@@ -1,14 +1,17 @@
 package dev.rlevkovych.addressbook.groupslistactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import dev.rlevkovych.addressbook.R
+import dev.rlevkovych.addressbook.contactslist.ContactsListActivity
 import dev.rlevkovych.addressbook.data.source.local.ContactsDataBase
 import kotlinx.android.synthetic.main.activity_groups_list.*
 import kotlinx.coroutines.CoroutineScope
@@ -23,9 +26,25 @@ class GroupsListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_groups_list)
-        viewModel = ViewModelProviders.of(this).get(GroupsListViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(GroupsListViewModel::class.java)
 
+        configRecyclerView()
+        configNavigation()
+    }
 
+    private fun configNavigation() {
+        display_all_contacts.setOnClickListener {
+            val intent = Intent(this, ContactsListActivity::class.java)
+            startActivity(intent)
+        }
+
+        display_contacts_by_group.setOnClickListener {
+            val intent = Intent(this, ContactsListActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun configRecyclerView() {
         groups_list.apply {
             layoutManager = LinearLayoutManager(this@GroupsListActivity)
             adapter = groupsAdapter
@@ -33,7 +52,6 @@ class GroupsListActivity : AppCompatActivity() {
 
         viewModel.availableGroups.observe(this, Observer {
             groupsAdapter.setData(it)
-            Toast.makeText(applicationContext, it.size.toString(), Toast.LENGTH_LONG).show()
         })
     }
 }
